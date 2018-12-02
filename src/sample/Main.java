3841fcf8b0acda174000f7b304af5fb304148b24
kserks777 <vladpoliakov777@mail.ru>
2018-12-02 13:12:54 +0300

    first push

---
+++ src/sample/Main.java
@@ -0,0 +1,24 @@
+package sample;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.layout.HBox;
+import javafx.stage.Stage;
+
+public class Main extends Application {
+
+    @Override
+    public void start(Stage primaryStage) throws Exception{
+        HBox root = new HBox();
+        primaryStage.setTitle("DB Editor");
+        primaryStage.setScene(new Scene(root, 800, 800));
+        primaryStage.show();
+    }
+
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
